60f54eb4811ccec551e652cfe1fc42f075bb2cec
Randall Mardus <randalljmardus@gmail.com>
2016-02-22 11:29:17 -0500

    dimension anchors with multipliers

--- Auto Layout Anchors/ViewController.swift
+++ Auto Layout Anchors/ViewController.swift
@@ -19,13 +19,19 @@ class ViewController: UIViewController {
         blueBox.backgroundColor = UIColor.blueColor()
         blueBox.translatesAutoresizingMaskIntoConstraints = false
         
-        blueBox.topAnchor.constraintEqualToAnchor(view.topAnchor).active = true
-        blueBox.leftAnchor.constraintEqualToAnchor(view.leftAnchor).active = true
-        blueBox.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
-        blueBox.rightAnchor.constraintEqualToAnchor(view.rightAnchor).active = true
+        //blueBox.topAnchor.constraintEqualToAnchor(view.topAnchor).active = true
+       // blueBox.leftAnchor.constraintEqualToAnchor(view.leftAnchor).active = true
+        //blueBox.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
+        //blueBox.rightAnchor.constraintEqualToAnchor(view.rightAnchor).active = true
         
+        //blueBox.widthAnchor.constraintEqualToConstant(200).active = true
+        //blueBox.heightAnchor.constraintEqualToConstant(100).active = true
         
-    
+        //blueBox.widthAnchor.constraintGreaterThanOrEqualToAnchor(view.widthAnchor, multiplier: 0.5).active = true
+        blueBox.heightAnchor.constraintGreaterThanOrEqualToAnchor(view.heightAnchor, multiplier: 0.5).active = true
+        
+        blueBox.widthAnchor.constraintGreaterThanOrEqualToAnchor(view.widthAnchor, multiplier: 0.5, constant: 80).active = true
+        
     
     }
 
